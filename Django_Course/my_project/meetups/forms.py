1161f6d5070e7b6c71ba89cfe8a2ea5ba31f1d9b
Muhammad Anas <91126991+MuhammadAnas789@users.noreply.github.com>
2023-04-06 00:39:24 +0500

    Delete Django_Course/my_project directory

--- Django_Course/my_project/meetups/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Participants
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model= Participants
-        fields=['email']
-    
--- Django_Course/my_project/meetups/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('meetups/', views.index, name='All_meetups'),
-    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail'),
-    path('meetups/success', views.confirm_registration, name='confirm-registration')
-]
